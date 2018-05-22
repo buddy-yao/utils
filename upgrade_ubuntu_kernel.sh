4301fa8e558b5863f885f2dbd0bc40900dd6286f
Buddy <yaokun@unnoo.com>
2018-05-23 02:39:37 +0800

    Update upgrade_ubuntu_kernel.sh

--- upgrade_ubuntu_kernel.sh
+++ upgrade_ubuntu_kernel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-ubuntu_16_04_kernel_version="4.4.0-124"
+ubuntu_16_04_kernel_version="4.4.0-125"
 ubuntu_14_04_kernel_version="3.13.0-147"
 
 version=$(lsb_release -r |awk '{print $2}')
